c1a201d2bbdc7147dbeeb03b76963651d0548580
marc2016 <marc@lammers.dev>
2026-05-23 21:46:50 +0200

    style: update app shell colors and theme for improved UI consistency

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { AppProviders } from "./components/providers/AppProviders";
 
 import "./i18n";
 
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
